cec49a36437902daf68d0888a4363980e6a7d986
SamvNoort <s.vannoort2@student.avans.nl>
2024-06-25 11:55:29 +0200

    added ;

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,24 +3,24 @@ const logger = require('../util/logger')
 
 let userController = {
     create: (req, res, next) => {
-        const user = req.body
-        logger.info('create user', user.firstName, user.lastName)
+        const user = req.body;
+        logger.info('create user', user.firstName, user.lastName);
         userService.create(user, (error, success) => {
             if (error) {
                 return next({
                     status: error.status,
                     message: error.message,
                     data: {}
-                })
+                });
             }
             if (success) {
                 res.status(200).json({
                     status: success.status,
                     message: success.message,
                     data: success.data
-                })
+                });
             }
-        })
+        });
     },
 
     getAll: (req, res, next) => {
@@ -65,22 +65,22 @@ let userController = {
     },
 
     getProfile: (req, res, next) => {
-        const userId = req.userId
-        logger.trace('getProfile for userId', userId)
+        const userId = req.userId;
+        logger.trace('getProfile for userId', userId);
         userService.getProfile(userId, (error, success) => {
             if (error) {
                 return next({
                     status: error.status,
                     message: error.message,
                     data: {}
-                })
+                });
             }
             if (success) {
                 res.status(200).json({
                     status: 200,
                     message: success.message,
                     data: success.data
-                })
+                });
             }
         })
     },
